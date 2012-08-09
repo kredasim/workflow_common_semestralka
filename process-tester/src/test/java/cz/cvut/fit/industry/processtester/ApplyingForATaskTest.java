8486c112f3c004d8079c726684d996a49d50ef15
veselda7 <veselda7@fit.cvut.cz>
2012-08-09 19:44:38 +0200

    Testing gate Selected - option apply.

--- process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
+++ process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
@@ -141,6 +141,22 @@ public class ApplyingForATaskTest extends JbpmJUnitTestCase {
 		assertNodeTriggered(processInstance.getId(), "Add to candidates");
 	}
 	
+	@Test
+	public void testSelectedApplyTest() {
+		Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("owner", "Pepik");
+		vars.put("selected", "apply");
+			
+		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
+			
+		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
+		executeHumanTask(taskService, "Pepik", LANG);
+		
+		assertNodeTriggered(processInstance.getId(), "Selected");
+		assertNodeTriggered(processInstance.getId(), "gate2");
+		assertNodeTriggered(processInstance.getId(), "Add to candidates");
+	}
+	
 	private void executeHumanTask(TaskService taskService, String owner, String lang){
 		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner(owner, lang);
 		TaskSummary task = list.get(0);
