02803c444694ae0413c79248a1ffd1a7e226e60b
veselda7 <veselda7@fit.cvut.cz>
2012-08-09 19:32:31 +0200

    Added test which test second branch of Selected gate - detail.

--- process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
+++ process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
@@ -78,6 +78,21 @@ public class ApplyingForATaskTest extends JbpmJUnitTestCase {
 		assertNodeTriggered(processInstance.getId(), "end");
 	}
 	
+	@Test
+	public void testSelectedDetailTest() {
+		Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("owner", "Pepik");
+		vars.put("selected", "detail");
+			
+		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
+			
+		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
+		executeHumanTask(taskService, "Pepik", LANG);
+		
+		assertNodeTriggered(processInstance.getId(), "Selected");
+		assertNodeTriggered(processInstance.getId(), "02 - View detail and apply for the task");
+	}
+	
 	private void executeHumanTask(TaskService taskService, String owner, String lang){
 		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner(owner, lang);
 		TaskSummary task = list.get(0);
