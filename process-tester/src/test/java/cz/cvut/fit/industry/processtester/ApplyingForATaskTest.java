6f7205145d42933157204ca02b51b2b0740fa649
veselda7 <veselda7@fit.cvut.cz>
2012-08-09 18:49:03 +0200

    Writing basic tests for executing first human task of the process.

--- process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
+++ process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
@@ -1,5 +1,9 @@
 package cz.cvut.fit.industry.processtester;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
@@ -7,6 +11,7 @@ import org.drools.io.ResourceFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.jbpm.task.TaskService;
+import org.jbpm.task.query.TaskSummary;
 import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,11 +41,32 @@ public class ApplyingForATaskTest extends JbpmJUnitTestCase {
 	
 	@Test
 	public void testProcessStart() {
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.AssignmentToCourse");		
+		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask");		
 		
 		assertProcessInstanceActive(processInstance.getId(), ksession);
 		assertNodeTriggered(processInstance.getId(), "gate1");
-		assertNodeTriggered(processInstance.getId(), "01 - Approve task");
-		assertNodeTriggered(processInstance.getId(), "Guarantator doesnt respond");
+		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
+	}
+	
+	
+	// Testing if every node is triggered after finishing first HumanTask
+	@Test
+	public void testLookForATaskFinishedTest() {
+		Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("owner", "Pepik");
+		vars.put("aproved", "denied");
+		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
+		executeHumanTask(taskService, "Pepik", LANG);
+		
+		assertNodeTriggered(processInstance.getId(), "Selected");
+	}
+	
+	private void executeHumanTask(TaskService taskService, String owner, String lang){
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner(owner, lang);
+		TaskSummary task = list.get(0);
+		System.out.println(task.getName());
+		System.out.println(owner + " is executing task: " + task.getName());
+		taskService.start(task.getId(), owner);
+		taskService.complete(task.getId(), owner, null);	// null jsou data, ktere dostaneme z tasku
 	}
 }
