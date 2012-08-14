e49cd20635f5137f3135a3686e9dcb8b966f2f86
veselda7 <veselda7@fit.cvut.cz>
2012-08-14 20:20:25 +0200

    Removing redundant code.

--- process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
+++ process-tester/src/test/java/cz/cvut/fit/industry/processtester/ApplyingForATaskTest.java
@@ -16,153 +16,141 @@ import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ApplyingForATaskTest extends JbpmJUnitTestCase {
-	
-	private static final String LANG = "en-UK";
-	private StatefulKnowledgeSession ksession;		// knowledge session for all tests
+public class ApplyingForATaskTest extends IndustryJUnitTestCase {
+
+	private static final String TASK_VIEW_DETAIL_AND_APPLY = "02 - View detail and apply for the task";
+	private static final String TASK_LOOK_FOR_A_TASK = "01 - Look for a task";
+	private static final String PROCESS_ID = "industry.impl.ApplyingForTask";
+	private static final String PROCESS_FILE_NAME = "02 - Applying for a task.bpmn2";
+	private StatefulKnowledgeSession ksession; // knowledge session for all
+												// tests
 	private TaskService taskService;
 
-	
 	public ApplyingForATaskTest() {
 		super(true);
 		setPersistence(true);
 	}
-	
+
 	@Before
 	public void getKnowlegdeSession() {
-		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-		kbuilder.add(ResourceFactory.newClassPathResource("02 - Applying for a task.bpmn2"), ResourceType.BPMN2);
-		
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource(PROCESS_FILE_NAME),
+				ResourceType.BPMN2);
+
 		kbuilder.newKnowledgeBase();
-				
-		ksession = createKnowledgeSession("02 - Applying for a task.bpmn2");
+
+		ksession = createKnowledgeSession(PROCESS_FILE_NAME);
 		taskService = getTaskService(ksession);
 	}
-	
+
 	@Test
 	public void testProcessStart() {
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask");		
-		
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID);
+
 		assertProcessInstanceActive(processInstance.getId(), ksession);
 		assertNodeTriggered(processInstance.getId(), "gate1");
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
+		assertNodeTriggered(processInstance.getId(), TASK_LOOK_FOR_A_TASK);
 	}
-	
-	
-	// Testing if every node is triggered after finishing first HumanTask
+
 	@Test
 	public void testLookForATaskFinished() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
-		
+		vars.put("owner", OWNER);
+
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+		executeHumanTask(taskService, OWNER, LANG, TASK_LOOK_FOR_A_TASK);
+
 		assertNodeTriggered(processInstance.getId(), "Selected");
 	}
-	
+
 	@Test
 	public void testSelectedEnd() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
+		vars.put("owner", OWNER);
 		vars.put("selected", "end");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
-		
+
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+
+		executeHumanTask(taskService, OWNER, LANG, TASK_LOOK_FOR_A_TASK);
+
 		assertNodeTriggered(processInstance.getId(), "Selected");
 		assertNodeTriggered(processInstance.getId(), "end");
 	}
-	
+
 	@Test
 	public void testSelectedDetail() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
+		vars.put("owner", OWNER);
 		vars.put("selected", "detail");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
+
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+
+		executeHumanTask(taskService, OWNER, LANG, TASK_LOOK_FOR_A_TASK);
 		
-		assertNodeTriggered(processInstance.getId(), "Selected");
-		assertNodeTriggered(processInstance.getId(), "02 - View detail and apply for the task");
+		assertNodeTriggered(processInstance.getId(),
+				TASK_VIEW_DETAIL_AND_APPLY);
 	}
-	
+
 	@Test
 	public void testSelectedDetailAppliedNo() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
+		vars.put("owner", OWNER);
 		vars.put("selected", "detail");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
-		
-		assertNodeTriggered(processInstance.getId(), "Selected");
-		assertNodeTriggered(processInstance.getId(), "02 - View detail and apply for the task");
-		
-		// executing "02 - View detail and apply for the task" humanTask
-		executeHumanTask(taskService, "Pepik", LANG);	
-		
-		assertNodeTriggered(processInstance.getId(), "Applied?");
-		
 		vars.put("applied", "no");
-		assertNodeTriggered(processInstance.getId(), "gate1");
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
+		
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+
+		executeHumanTask(taskService, OWNER, LANG, TASK_LOOK_FOR_A_TASK);
+		executeHumanTask(taskService, OWNER, LANG, TASK_VIEW_DETAIL_AND_APPLY);
+
+		assertNodeTriggered(processInstance.getId(), "Applied?");
 	}
-	
+
 	@Test
 	public void testSelectedDetailAppliedYes() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
+		vars.put("owner", OWNER);
 		vars.put("selected", "detail");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
-		
+
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+
+		assertNodeTriggered(processInstance.getId(), TASK_LOOK_FOR_A_TASK);
+		executeHumanTask(taskService, OWNER, LANG);
+
 		assertNodeTriggered(processInstance.getId(), "Selected");
-		assertNodeTriggered(processInstance.getId(), "02 - View detail and apply for the task");
-		
+		assertNodeTriggered(processInstance.getId(),
+				TASK_VIEW_DETAIL_AND_APPLY);
+
 		// executing "02 - View detail and apply for the task" humanTask
-		executeHumanTask(taskService, "Pepik", LANG);	
-		
+		executeHumanTask(taskService, OWNER, LANG);
+
 		assertNodeTriggered(processInstance.getId(), "Applied?");
-		
+
 		vars.put("applied", "yes");
 		assertNodeTriggered(processInstance.getId(), "gate2");
 		assertNodeTriggered(processInstance.getId(), "Add to candidates");
 	}
-	
+
 	@Test
 	public void testSelectedApply() {
 		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("owner", "Pepik");
+		vars.put("owner", OWNER);
 		vars.put("selected", "apply");
-			
-		ProcessInstance processInstance = ksession.startProcess("industry.impl.ApplyingForTask", vars);		
-			
-		assertNodeTriggered(processInstance.getId(), "01 - Look for a task");
-		executeHumanTask(taskService, "Pepik", LANG);
-		
+
+		ProcessInstance processInstance = ksession.startProcess(PROCESS_ID,
+				vars);
+
+		assertNodeTriggered(processInstance.getId(), TASK_LOOK_FOR_A_TASK);
+		executeHumanTask(taskService, OWNER, LANG);
+
 		assertNodeTriggered(processInstance.getId(), "Selected");
 		assertNodeTriggered(processInstance.getId(), "gate2");
 		assertNodeTriggered(processInstance.getId(), "Add to candidates");
-	}
-	
-	private void executeHumanTask(TaskService taskService, String owner, String lang){
-		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner(owner, lang);
-		TaskSummary task = list.get(0);
-		System.out.println(task.getName());
-		System.out.println(owner + " is executing task: " + task.getName());
-		taskService.start(task.getId(), owner);
-		taskService.complete(task.getId(), owner, null);	// null jsou data, ktere dostaneme z tasku
 	}
 }
