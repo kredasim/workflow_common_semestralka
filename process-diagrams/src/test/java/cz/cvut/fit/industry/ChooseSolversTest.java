a7dec80d1a1b46a91e944c8461f0c55b0b0f54e9
veselda7 <veselda7@fit.cvut.cz>
2012-10-17 11:41:33 +0200

    Testing entering nodes.

--- process-diagrams/src/test/java/cz/cvut/fit/industry/ChooseSolversTest.java
+++ process-diagrams/src/test/java/cz/cvut/fit/industry/ChooseSolversTest.java
@@ -3,7 +3,9 @@ package cz.cvut.fit.industry;
 import static org.junit.Assert.assertNotNull;
 
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.activiti.engine.test.Deployment;
 import org.junit.Test;
 
@@ -17,6 +19,46 @@ public class ChooseSolversTest extends AbstractActivitiTest{
 		RuntimeService runtimeService = activitiRule.getRuntimeService();
 		ProcessInstance instance = runtimeService.startProcessInstanceByKey(DEFINITION_KEY);
 		assertNotNull(instance.getId());
+	}
+	
+	@Test
+	@Deployment(resources = {"diagrams/03-ChooseSolvers.bpmn"})
+	public void processShouldExecuteEnterTask01ChooseSolvers() {
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		ProcessInstance instance = runtimeService.startProcessInstanceByKey(DEFINITION_KEY);
+		assertNodeVisited(instance, "usertask1");
+	}
+	
+	@Test
+	@Deployment(resources = {"diagrams/03-ChooseSolvers.bpmn"})
+	public void processShouldExecuteEnterInitiateTask() {
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		TaskService taskService = activitiRule.getTaskService();
+		ProcessInstance instance = runtimeService.startProcessInstanceByKey(DEFINITION_KEY);
+		
+		Task taskInstance = taskService.createTaskQuery().taskDefinitionKey("usertask1")
+				.processInstanceId(instance.getId()).singleResult();
+		
+		runtimeService.setVariable(instance.getId(), "exclusiveGatewayDesicion", "complete");
+		
+		taskService.complete(taskInstance.getId());
+				
+		assertNodeVisited(instance, "scripttask1");
+	}
+	
+	@Test
+	@Deployment(resources = {"diagrams/03-ChooseSolvers.bpmn"})
+	public void processShouldExecuteEnterTaskCanceled() {
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		TaskService taskService = activitiRule.getTaskService();
+		ProcessInstance instance = runtimeService.startProcessInstanceByKey(DEFINITION_KEY);
+		
+		Task taskInstance = taskService.createTaskQuery().taskDefinitionKey("usertask1")
+				.processInstanceId(instance.getId()).singleResult();
+		
+		runtimeService.setVariable(instance.getId(), "exclusiveGatewayDesicion", "canceled");		
+		taskService.complete(taskInstance.getId());			
+		assertNodeVisited(instance, "endevent1");
 	}
 
 }
