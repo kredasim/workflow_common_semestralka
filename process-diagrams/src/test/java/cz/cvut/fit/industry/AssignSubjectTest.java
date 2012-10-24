da495712a2e3337eac91a520d05e981f166ad427
Vojta Tošovský <tosovvoj@fit.cvut.cz>
2012-10-24 14:44:09 +0200

    AssignCourseTest

--- process-diagrams/src/test/java/cz/cvut/fit/industry/AssignSubjectTest.java
+++ process-diagrams/src/test/java/cz/cvut/fit/industry/AssignSubjectTest.java
@@ -2,11 +2,13 @@ package cz.cvut.fit.industry;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AssignSubjectTest extends AbstractActivitiTest {
 
 	@Test
+	@Ignore
 	public void test() {
 		fail("Not yet implemented");
 	}
