ad277e53f94057b274cfa9b77497d2bade91046a
Vojta Tošovský <tosovvoj@fit.cvut.cz>
2012-11-14 12:11:50 +0100

    Ldap conector

---
+++ industry-activiti-library/src/main/java/cz/cvut/fit/industry/IndustryException.java
@@ -0,0 +1,27 @@
+package cz.cvut.fit.industry;
+
+public class IndustryException extends RuntimeException {
+
+	public IndustryException() {
+	}
+
+	public IndustryException(String arg0) {
+		super(arg0);
+	}
+
+	public IndustryException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public IndustryException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public IndustryException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
+
+
