995432caf2d4fb313c8e0c8ffb753152322d692d
ligasm <fdfs>
2012-11-22 14:26:46 +0100

    improved library initialization; added workflow service

---
+++ workflow-common/src/main/java/cz/cvut/fit/workflow/common/WorkflowServiceImpl.java
@@ -0,0 +1,60 @@
+/* ===========================================================================
+ * IBA CZ Confidential
+ *
+ * (c) Copyright IBA CZ 2011 ALL RIGHTS RESERVED
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets.
+ *
+ * =========================================================================== */
+package cz.cvut.fit.workflow.common;
+
+import cz.cvut.fit.industry.api.service.ActivitiLibrary;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
+ */
+
+@Service("workflowService")
+public class WorkflowServiceImpl implements WorkflowService {
+    private static final Logger LOG = LoggerFactory.getLogger(WorkflowServiceImpl.class);
+    @Autowired
+    private ActivitiLibrary activitiLibrary;
+
+    @Override
+    public String createTask(String processKey) {
+        String result = null;
+        try {
+            LOG.info("Creating task");
+            RuntimeService runtimeService = activitiLibrary.getRuntimeService();
+            result = runtimeService.startProcessInstanceByKey(processKey).getId();
+        } catch (Exception e) {
+            LOG.warn("Error occurred.", e);
+        }
+        return result;
+    }
+
+    @Override
+    public void completeTaskByProcessId(String processId, Map<String, Object> data) {
+        try {
+            // Get the first task
+            TaskService taskService = activitiLibrary.getTaskService();
+            TaskQuery taskQuery = taskService.createTaskQuery()
+                    .processInstanceId(processId);
+
+            Task task = taskQuery.singleResult();
+            taskService.complete(task.getId(), data);
+        } catch (Exception e) {
+            LOG.warn("Error occurred.", e);
+        }
+    }
+}
---
+++ workflow-common/src/test/java/cz/cvut/fit/workflow/common/InitializationTest.java
@@ -0,0 +1,27 @@
+/* ===========================================================================
+ * IBA CZ Confidential
+ *
+ * (c) Copyright IBA CZ 2011 ALL RIGHTS RESERVED
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets.
+ *
+ * =========================================================================== */
+package cz.cvut.fit.workflow.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:workflow-common.xml"})
+public class InitializationTest {
+    @Test
+    public void testGetActivitiLibrary() throws Exception {
+
+    }
+}
