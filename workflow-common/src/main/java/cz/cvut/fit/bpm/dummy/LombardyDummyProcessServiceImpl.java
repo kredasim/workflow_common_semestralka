f6eb94c97aeefb69ecea9ec0766f1cf922213e75
miroslav.ligas <miroslav.ligas@ibacz.eu>
2012-12-06 23:27:04 +0100

    activiti conector

--- workflow-common/src/main/java/cz/cvut/fit/bpm/dummy/LombardyDummyProcessServiceImpl.java
+++ workflow-common/src/main/java/cz/cvut/fit/bpm/dummy/LombardyDummyProcessServiceImpl.java
@@ -8,55 +8,20 @@
  * =========================================================================== */
 package cz.cvut.fit.bpm.dummy;
 
-import cz.cvut.fit.bpm.api.dto.BpmTaskDto;
-import cz.cvut.fit.bpm.api.service.TaskService;
+import cz.cvut.fit.bpm.api.dto.BpmProcessDto;
+import cz.cvut.fit.bpm.api.service.ProcessService;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
  */
 @Component
-public class LombardyDummyProcessServiceImpl implements TaskService {
-    @Override
-    public List<BpmTaskDto> getAllTasksForUser(String userId) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
+public class LombardyDummyProcessServiceImpl implements ProcessService {
 
     @Override
-    public List<BpmTaskDto> getAssignedTasksForUser(String userId) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public List<BpmTaskDto> getAvailableTasksForUser(String userId) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String startTask(String processId) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void completeTask(String taskId) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void completeTask(String taskId, Map<String, Object> data) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void completeTask(String processId, String userId, Map<String, Object> data) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public BpmTaskDto getTaskById(String taskId) {
+    public List<BpmProcessDto> getAllProcessesForUser(String userId) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
